6a34a7f5e57e8578b8468ddbc15e104537fb8f06
sujanthakare <sujandt@gmail.com>
2018-06-17 14:28:27 +0530

    added simple class

--- handler.ts
+++ handler.ts
@@ -1,11 +1,14 @@
 import { Handler, Context, Callback } from "aws-lambda";
+import Logger from "./src/Logger";
 
+let _logger = new Logger();
 interface HelloResponse {
   statusCode: number;
   body: string;
 }
 
 const webhook: Handler = (event: any, context: Context, callback: Callback) => {
+  _logger.log("This is message");
   const response: HelloResponse = {
     statusCode: 200,
     body: JSON.stringify({
